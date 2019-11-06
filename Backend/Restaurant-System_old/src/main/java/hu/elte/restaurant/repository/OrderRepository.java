479381de311d1d592ef8a8cdbdd9ba4c28086c45
Dávid <petrik.david1@gmail.com>
2019-11-06 21:53:56 +0100

    Régi verzió eltávolitva

--- Backend/Restaurant-System_old/src/main/java/hu/elte/restaurant/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.elte.restaurant.repository;
-
-import hu.elte.restaurant.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Long>
-{
-
-}
